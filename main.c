dfba3140b5b4950c145fb26e88fe44d43653ae73
alumno <vzalberto@msn.com>
2015-01-30 20:40:06 -0600

    faltan 2

--- main.c
+++ main.c
@@ -6,22 +6,40 @@ void tratar_alarma(int signum){
     printf("Numero de señal: %d\n", signum);
 }
 
+void tratar_SIGTSTP(int signum){
+    printf("La señal %d me ha intentado detener. Ilusa.\n", signum);
+}
+
+void cant_touch_this(int signum){
+    printf("Don't. Stop. Me. Now. Cause I'm having a good time.\n");
+}
+
 int main(void){
-    struct sigaction act;
+    struct sigaction act, act2, act3;
     sigset_t mask;
 
     act.sa_handler = tratar_alarma;
     act.sa_flags = 0;
 
-    sigemptyset(&mask);
-    sigprocmask(SIG_SETMASK, &mask, NULL);
-    sigaction(SIGALRM, &act, NULL);
+    act2.sa_handler = tratar_SIGTSTP;
+    act2.sa_flags = 0;
 
-    if(sigismember(&mask, SIGQUIT))
-        sigdelset(&mask, SIGQUIT);
+    act3.sa_handler = cant_touch_this;
+    act3.sa_flags = 0;
+
+    printf("PID: %d\n", getpid());
+
+    sigemptyset(&mask);
+    
+    sigfillset(&mask);
+    sigaddset(&mask, 18);
+    sigprocmask(SIG_SETMASK, &mask, NULL);
+
+
+    sigaction(SIGALRM, &act, NULL);
+    sigaction(18, &act3, NULL);
 
     for(;;){
-        //ualarm(500000, 0);
         alarm(3);
         pause();
     }
